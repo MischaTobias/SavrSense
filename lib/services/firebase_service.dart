e3fa0e7705c59161620533aa1cd08f097a4401d5
Mischa Tobias <mischa.tobias4b@gmail.com>
2025-04-11 18:23:30 -0600

    firebase fix

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
-import '../firebase_options.dart';
+import '../core/config/firebase_options.dart';
 
 class FirebaseService {
   static Future<void> initializeFirebase() async {
